c0c248f878230a21a1f080b1b880bf5665f8c6f7
amm1746 <amm1746@rit.edu>
2025-09-19 11:51:49 -0400

    Improved mobile formatting

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,6 +12,13 @@ const Hero = () => {
         pipelines, backend services, data-driven tools, and more. My projects focus on efficiency 
         and solving real-world problems. 
       </p>
+
+        <div className="hero-buttons">
+        <a href="#projects" className="btn">View Projects</a>
+        <a href="/Mantagas_Alexandra_Resume_CS_BS.pdf" className="btn" target="_blank" rel="noopener noreferrer">
+          Download Resume
+        </a>
+      </div>
     </section>
   );
 };
